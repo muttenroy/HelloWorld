ccd2fd6776db6418e85e86e86879159705819379
muttenroy <38403404+muttenroy@users.noreply.github.com>
2018-11-09 09:43:16 +0100

    Prueba Unitaria

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -6,9 +6,24 @@ namespace UnitTestProject1
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        [TestMethod()]
+        public void SumarTest()
         {
+            ClaseEjemplo target = new ClaseEjemplo();
+            int a = 1;
+            int b = 2;
+            int expected = 3;
+            int actual;
+            actual = target.Sumar(a, b);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+
+    internal class ClaseEjemplo
+    {
+        internal int Sumar(int a, int b)
+        {
+            return (a + b);
         }
     }
 }
