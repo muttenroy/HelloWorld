2c41723490c7b3f2b1714527e35a6dd0a8d1f344
muttenroy <38403404+muttenroy@users.noreply.github.com>
2018-11-09 14:18:29 +0100

    Nunit tests

---
+++ NUnitTestProject1/NUnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace NUnitTestProject1
+{
+    //[TestFixture]
+    public class NUnitTest1
+    {
+        //[Test]
+        public void SumarTest()
+        {
+            ClaseEjemplo target = new ClaseEjemplo();
+            int a = 1;
+            int b = 2;
+            int expected = 3;
+            int actual;
+            actual = target.Sumar(a, b);
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        internal class ClaseEjemplo
+        {
+            internal int Sumar(int a, int b)
+            {
+                return (a + b);
+            }
+        }
+    }
+}
